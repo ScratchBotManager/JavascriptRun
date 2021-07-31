0fe44f03155d691063356ea02362c24694f202ab
ScratchBotManager <84136976+ScratchBotManager@users.noreply.github.com>
2021-07-31 15:46:56 -0400

    Update CustomFunctions.js

--- CustomFunctions.js
+++ CustomFunctions.js
@@ -1,6 +1,3 @@
-function dice(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  var Random = Math.floor(Math.random() * (max - min) + min);
-  return alert(Random)
+function Open(url) {
+ window.open(url) 
 }
